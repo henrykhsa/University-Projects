082fdd04f809399aab86f49db2e56e12b5560141
Henry <henryk33658@gmail.com>
2025-05-25 01:03:23 -0300

    25052025.2

--- xadrez.c
+++ xadrez.c
@@ -2,9 +2,22 @@
 
 int main () {
    
-    for (int torre = 0; torre <= 4; torre++) {
+    int Torre = 0;
+    int Bispo = 0;
+
+    printf("Movimentos da torre\n\n");
+    for (Torre; Torre <= 4; Torre++) {
         printf("direita\n");
     }
 
+    printf("\nMovimentos do bispo\n\n");
+    while (Bispo <= 4) {
+        printf("cima\n");
+        printf("direita\n");
+        Bispo++;
+    }
+
+    Bispo++;
+    
     return 0;
 }
