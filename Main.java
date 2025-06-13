01599204dccb5add03de7d05c72e8c11ad56f64e
Henry <henryk33658@gmail.com>
2025-06-13 03:57:26 -0300

    13062025.1

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
 abstract class Animal {
     // Método abstrato que deve ser implementado pelas subclasses
     public abstract void makeSound();
+    public abstract void TypeOfAnimal();
     // Método concreto que pode ser usado por todas as subclasses
     public void sleep() {
         System.out.println("ZZZzzz...");
@@ -15,6 +16,9 @@ class Dog extends Animal {
     public void makeSound() {
         System.out.println("Woof!");
     }
+    public void TypeOfAnimal() {
+        System.out.println("É um animal doméstico!");
+    }
 }
 
 class Cat extends Animal {
@@ -22,20 +26,38 @@ class Cat extends Animal {
     public void makeSound() {
         System.out.println("Meow!");
     }
+    public void TypeOfAnimal() {
+        System.out.println("É um animal doméstico!");
+    }
+}
+
+class Lion extends Animal {
+    // Implementação do método makeSound para a classe Lion
+    public void TypeOfAnimal() {
+        System.out.println("É um animal selvagem!");
+    }
+    public void makeSound() {
+        System.out.println("Roar!");
+    }
 }
 
 // Classe principal para testar a hierarquia de classes
 public class Main {
     public static void main(String[] args) {
         // Criando instâncias de Dog e Cat
-        Animal myDog = new Dog();
-        Animal myCat = new Cat();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+        Animal lion = new Lion();
 
         // Chamando os métodos makeSound e sleep
-        myDog.makeSound(); // Saída: Woof!
-        myDog.sleep();     // Saída: ZZZzzz...
+        dog.makeSound(); // Saída: Woof!
+        dog.sleep();     // Saída: ZZZzzz...
 
-        myCat.makeSound(); // Saída: Meow!
-        myCat.sleep();     // Saída: ZZZzzz...
+        cat.makeSound(); // Saída: Meow!
+        cat.sleep();     // Saída: ZZZzzz...
+
+        lion.makeSound(); // Saída: Roar!
+        lion.sleep();     // Saída: ZZZzzz...
+        lion.TypeOfAnimal(); // Saída: É um animal selvagem!
     }
 }
